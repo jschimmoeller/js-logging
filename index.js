8a373a30fc1493a91adaf412c4edb217eedca856
jschimmoeller <jschimmoeller@star2star.com>
2017-06-29 13:10:31 -0400

    message formatter

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ var logger = new (winston.Logger)({
       formatter: function (options) {
         //console.log('args:', options)
         // Return string will be passed to logger.
+        // ['level=${options.level} title=${options.message} metadata=${newMeta} ]
+
+        // var newMeta = options.meta;
+        //fullMeta.id && delete newMeta.id;
+
         return  "custom stuff here: "+options.message;
       },
     })
   ]
 });
 
-logger.log('tracer', 'test tracer', 1,2,3, {a:1,b:2,c:'a'});
+logger.log('tracer', 'test tracer', {a:1,b:2,c:'a', id:1, trace: 'a', parent: '0'});
 logger.log('debug', 'test debug');
 logger.tracer('a');
